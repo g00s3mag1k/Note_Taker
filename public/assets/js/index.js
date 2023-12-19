d7db138dc8879f8f9980312a155677d1d2d8ce4b
Brandon <beegee54321@gmail.com>
2023-12-19 13:29:01 -0800

    finished index.js and style.css still need to add routes and resolve functionality

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -71,9 +71,29 @@ const handleNoteSave = () => {
     });
 };
 
+const handleNoteDelete = (e) => {
+    e.stopPropagation();
+    const note = e.target;
+    const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  
+    if (activeNote.id === noteId) {
+      activeNote = {};
+    }
+  
+    deleteNote(noteId).then(() => {
+      getAndRenderNotes();
+      renderActiveNote();
+    });
+};
+
 const handleNoteView = (e) => {
     e.preventDefault();
     activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+    renderActiveNote();
+};
+
+const handleNewNoteView = () => {
+    activeNote = {};
     renderActiveNote();
 };
 
@@ -85,3 +105,64 @@ const handleRenderSaveBtn = () => {
     }
 };
 
+const renderNoteList = async (notes) => {
+    let jsonNotes = await notes.json();
+    if (window.location.pathname === '/notes') {
+      noteList.forEach((el) => (el.innerHTML = ''));
+    }
+  
+    let noteListItems = [];
+  
+const createLi = (text, delBtn = true) => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('list-group-item');
+  
+    const spanEl = document.createElement('span');
+    spanEl.innerText = text;
+    spanEl.addEventListener('click', handleNoteView);
+  
+    liEl.append(spanEl);
+  
+    if (delBtn) {
+        const delBtnEl = document.createElement('i');
+        delBtnEl.classList.add(
+        'fas',
+        'fa-trash-alt',
+        'float-right',
+        'text-danger',
+        'delete-note'
+    );
+    delBtnEl.addEventListener('click', handleNoteDelete);
+  
+        liEl.append(delBtnEl);
+    }
+  
+    return liEl;
+};
+  
+    if (jsonNotes.length === 0) {
+    noteListItems.push(createLi('No saved Notes', false));
+}
+  
+    jsonNotes.forEach((note) => {
+        const li = createLi(note.title);
+        li.dataset.note = JSON.stringify(note);
+  
+    noteListItems.push(li);
+});
+  
+    if (window.location.pathname === '/notes') {
+        noteListItems.forEach((note) => noteList[0].append(note));
+}
+};
+
+const getAndRenderNotes = () => getNotes().then(renderNoteList);
+  
+    if (window.location.pathname === '/notes') {
+        saveNoteBtn.addEventListener('click', handleNoteSave);
+        newNoteBtn.addEventListener('click', handleNewNoteView);
+        noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+        noteText.addEventListener('keyup', handleRenderSaveBtn);
+}
+
+getAndRenderNotes();
