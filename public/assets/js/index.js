b85b0e96dcb04f7699af95809bc100390c98ff9c
Brandon <beegee54321@gmail.com>
2023-12-19 02:32:32 -0800

    added boiler to route files and started adding to index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,40 @@
-const express = require('express');
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
 
-//const router = express.Router();
+if (window.location.pathname === '/notes') {
+  noteTitle = document.querySelector('.note-title');
+  noteText = document.querySelector('.note-textarea');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  noteList = document.querySelectorAll('.list-container .list-group');
+}
 
-//const 
-//const
+const show = (elem) => {
+    elem.style.display = 'inline';
+};
 
-//router.use()
-//router.use()
+const hide = (elem) => {
+    elem.style.display = 'none';
+};
 
-//module.exports = router;
+let activeNote = {};
+
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+});
+
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+});
---
+++ routes/htmlRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+//const router = express.Router();
+
+//const 
+//const
+
+//router.use()
+//router.use()
+
+//module.exports = router;
